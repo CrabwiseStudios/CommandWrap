abe6d50325111ddffc5b0bf4a1ad08f228b33c35
Greg <gcurtis@crabwisestudios.com>
2010-03-20 19:24:13 -0400

    Created syntax attributes

--- Crabwise.CommandWrap.Library/Command.cs
+++ Crabwise.CommandWrap.Library/Command.cs
@@ -1,5 +1,7 @@
 ﻿namespace Crabwise.CommandWrap.Library
 {
+    using System;
+
     /// <summary>
     /// Provides an abstract representation of a command prompt command.
     /// </summary>
@@ -39,6 +41,87 @@
         public override string ToString()
         {
             return base.ToString();
+        }
+
+        /// <summary>
+        /// Describes the syntax of a command.
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+        public sealed class CommandSyntaxAttribute : SyntaxAttribute
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="CommandSyntaxAttribute"/> class.
+            /// </summary>
+            /// <param name="syntax">The syntax of the command.</param>
+            public CommandSyntaxAttribute(string syntax) : base(syntax)
+            {
+            }
+
+            /// <summary>
+            /// Gets or sets the default path for the command.
+            /// </summary>
+            public string DefaultPath { get; set; }
+
+            /// <summary>
+            /// Gets or sets the default working directory for the command.
+            /// </summary>
+            public string DefaultWorkingDirectory { get; set; }
+        }
+
+        /// <summary>
+        /// Describes the syntax of a command parameter.
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+        public sealed class ParameterSyntaxAttribute : SyntaxAttribute
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ParameterSyntaxAttribute"/> class.
+            /// </summary>
+            /// <param name="syntax">The syntax of the parameter.</param>
+            public ParameterSyntaxAttribute(string syntax)
+                : base(syntax)
+            {
+            }
+
+            /// <summary>
+            /// Gets or sets the ordering priority of this parameter. Parameters with higher priorities are placed 
+            /// first in the command prompt string.
+            /// </summary>
+            public int Priority { get; set; }
+
+            /// <summary>
+            /// Gets or sets a value indicating whether this parameter is required.
+            /// </summary>
+            public bool Required { get; set; }
+        }
+
+        /// <summary>
+        /// Describes the syntax of a command prompt element.
+        /// </summary>
+        [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+        public class SyntaxAttribute : Attribute
+        {
+            /// <summary>
+            /// Syntax of the element.
+            /// </summary>
+            private readonly string syntax;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SyntaxAttribute"/> class.
+            /// </summary>
+            /// <param name="syntax">The syntax of the element.</param>
+            public SyntaxAttribute(string syntax)
+            {
+                this.syntax = syntax;
+            }
+
+            /// <summary>
+            /// Gets the syntax of the element.
+            /// </summary>
+            public string Syntax
+            {
+                get { return this.syntax; }
+            }
         }
     }
 }
